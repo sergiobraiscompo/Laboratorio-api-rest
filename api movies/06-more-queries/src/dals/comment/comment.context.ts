ca97ceef0def7984c7d1470d695ed49686881ab0
sergiobraiscompo <sergio>
2024-12-04 19:59:58 +0100

    reestructuración carpetas

--- api movies/06-more-queries/src/dals/comment/comment.context.ts
+++
@@ -1,5 +0,0 @@
-import { dbServer } from '#core/servers/index.js';
-import { Comment } from './comment.model.js';
-
-export const getCommentContext = () =>
-  dbServer.db?.collection<Comment>('comments');
