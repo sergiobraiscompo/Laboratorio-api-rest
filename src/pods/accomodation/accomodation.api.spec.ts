4780be764e5265f072026b69a113a0afc918bf45
Sergio <comesanapoloniosergiobrais@gmail.com>
2025-09-23 12:46:51 +0200

    refactor PUTReview and test Put Review

--- src/pods/accomodation/accomodation.api.spec.ts
+++ src/pods/accomodation/accomodation.api.spec.ts
@@ -323,28 +323,27 @@ describe('pods/accomodation/accomodation.api specs', () => {
 
   // put tests
   describe('inserts a review on the specified accomdation', () => {
-    it('should return 201 when review is inserted', async () => {
-      // Arrange
-      const route = '/65097600a74000a4a4a22687';
-      const newReview = {
-        "listing_id": "10006546",
-        "reviewer_id": "000000000",
-        "reviewer_name": "notocold",
-        "comments": "Lorem ipsum dolor amet."
-      };
-
-      // Act
-      const response = await supertest(app).put(route).send(newReview);
-
-      // Assert
-      expect(response.statusCode).toEqual(201);
-      expect(response.body.id).toEqual(expect.any(String));
-      expect(response.body._id).toBe(Number);
-      expect(response.body.date).toBe(Date);
-      expect(response.body.listing_id).toEqual(newReview.listing_id);
-      expect(response.body.reviewer_id).toEqual(newReview.reviewer_id);
-      expect(response.body.reviewer_name).toEqual(newReview.reviewer_name);
-      expect(response.body.comments).toEqual(newReview.comments);
-    });
+  it("should return 201 when review is inserted", async () => {
+       // Arrange
+       const route = "/65097600a74000a4a4a22687";
+       const newReview = {
+         listing_id: "10006546",
+         reviewer_id: "000000000",
+         reviewer_name: "notocold",
+         comments: "Lorem ipsum dolor amet.",
+       };
+ 
+       // Act
+       const response = await supertest(app).put(route).send(newReview);
+ 
+       // Assert
+       expect(response.statusCode).toEqual(201);
+       expect(typeof response.body._id).toBe("number");
+       expect(new Date(response.body.date)).toBeInstanceOf(Date);
+       expect(response.body.listing_id).toEqual(newReview.listing_id);
+       expect(response.body.reviewer_id).toEqual(newReview.reviewer_id);
+       expect(response.body.reviewer_name).toEqual(newReview.reviewer_name);
+       expect(response.body.comments).toEqual(newReview.comments);
+     });
+   });
   });
-});
--- src/pods/accomodation/accomodation.api.ts
+++ src/pods/accomodation/accomodation.api.ts
@@ -30,22 +30,29 @@ accomodationApi
       next(error);
     }
   })
-  .put("/:id", async (req, res, next) => {
-    try {
-      const { id } = req.params;
-      const accomodation = await accomodationRepository.getAccomodation(id);
-      if (accomodation) {
-        const lastId = accomodation.reviews.sort((reviewA, reviewB) => Number(reviewA._id) - Number(reviewB._id)).slice(0)[0]._id;
-        const newReview: Review = { _id: lastId + 1, date: new Date(), ...req.body };
-        await accomodationRepository.addReview(accomodation, newReview);
-        res.sendStatus(201).send(accomodation);
-      } else {
-        res.sendStatus(404);
-      }
-    } catch (error) {
-      next(error);
-    }
-  })
+ .put("/:id", async (req, res, next) => {
+     try {
+       const { id } = req.params;
+       const accomodation = await accomodationRepository.getAccomodation(id);
+       if (accomodation) {
+         const lastId =
+           accomodation.reviews && accomodation.reviews.length > 0
+             ? Math.max(...accomodation.reviews.map((r) => Number(r._id)))
+             : 0;
+         const newReview: Review = {
+           _id: lastId + 1,
+           date: new Date(),
+           ...req.body,
+         };
+         await accomodationRepository.addReview(accomodation, newReview);
+         res.status(201).send(newReview);
+       } else {
+         res.sendStatus(404);
+       }
+     } catch (error) {
+       next(error);
+     }
+   })
   .delete("/:id", async(req, res, next) => {
     try {
       const { id } = req.params;
